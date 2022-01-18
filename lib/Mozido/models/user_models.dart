703adac3dc23286439303d2ee756eedc0eab9b96
stenkjan <m.janstenk@gmail.com>
2022-01-18 18:33:19 +0100

    fixed user model

--- lib/Mozido/models/user_models.dart
+++ lib/Mozido/models/user_models.dart
@@ -1,22 +1,22 @@
 class User {
-  final String id;
-  final String fullName;
-  final String email;
-  final String userRole;
-  // User({this.id, this.fullName, this.email, this.userRole});
-
+  final String? id;
+  // final String fullName;
+  final String? email;
+  // final String userRole;
+  User(this.id, this.email);
+  // User({required this.id, required this.fullName, required this.email, required this.userRole});
   User.fromData(Map<String, dynamic> data)
       : id = data['id'],
-        fullName = data['fullName'],
-        email = data['email'],
-        userRole = data['userRole'];
-  // Map<String, dynamic> toJson() {
+        //fullName = data['fullName'],
+        email = data['email'];
+  // userRole = data['userRole'];
+
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'fullName': fullName,
+      //'fullName': fullName,
       'email': email,
-      'userRole': userRole,
+      // 'userRole': userRole,
     };
   }
 }
