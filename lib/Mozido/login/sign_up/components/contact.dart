3a77c6f266f25bfd4f3b41981b5ca54e2ab87db5
stenkjan <m.janstenk@gmail.com>
2022-01-20 21:07:04 +0100

    vopied contact class to signup

--- lib/Mozido/login/sign_up/components/contact.dart
+++ lib/Mozido/login/sign_up/components/contact.dart
@@ -4,6 +4,7 @@ import 'package:covidapp/Mozido/login/sign_in/signin.dart';
 import 'package:covidapp/Mozido/login/sign_in/components/credentials.dart';
 import 'package:covidapp/Mozido/login/widgets/account_check.dart';
 import 'package:covidapp/Mozido/login/widgets/rounded_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'credentials.dart';
 
@@ -14,18 +15,58 @@ class Social extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          'oder kontaktieren Sie uns',
-          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+        SizedBox(
+          height: 30,
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: appPadding),
-          child: RoundedButton(imageSrc: 'images/mail.png', press: () {}),
+        Center(
+          child: TextButton(
+              child: const Text(
+                'oder kontaktieren Sie uns  \u{1F4E7}',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+              ),
+              onPressed: () {
+                _launchURL();
+              }),
         ),
-        const SizedBox(
+        SizedBox(
           height: appPadding,
         ),
+        /*  AccountCheck(
+          login: true,
+          press: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return SignUpScreen();
+                },
+              ),
+            );
+          },
+        ), */
       ],
     );
+  }
+
+  String? encodeQueryParameters(Map<String, String> params) {
+    return params.entries
+        .map((e) =>
+            '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .join('&');
+  }
+
+  void _launchURL() async {
+    final Uri emailLaunchUri = Uri(
+        scheme: 'mailto',
+        path: 'jan.stenk@edu.fh-joanneum.at',
+        query: encodeQueryParameters(
+            <String, String>{'Long Covid App Support': 'Ihr Anliegen:'}));
+    //  launch(emailLaunchUri.toString());
+    // if (!await launch("mailto:")) throw 'Could not launch $_url';
+    if (await canLaunch(emailLaunchUri.toString())) {
+      await launch(emailLaunchUri.toString());
+    } else {
+      throw 'Could not launch $Uri';
+    }
   }
 }
